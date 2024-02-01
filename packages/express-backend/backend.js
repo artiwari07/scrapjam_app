aa20a907fdcf9b7f68b472fe11784d865f5f69ce
leah16303 <leahsarles@gmail.com>
2024-01-31 20:47:37 -0800

    added users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,3 +15,37 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
