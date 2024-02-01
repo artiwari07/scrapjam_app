e56966bb79667b17e6438e90e7fc0361c1b72939
leah16303 <leahsarles@gmail.com>
2024-01-31 20:42:00 -0800

    backend created

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,17 @@
+// backend.js
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
