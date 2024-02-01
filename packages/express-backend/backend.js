5429f034812ad4e18dac936157abfe8818a04129
leah16303 <leahsarles@gmail.com>
2024-01-31 21:24:10 -0800

    added post

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -63,3 +63,14 @@ app.get("/users/:id", (req, res) => {
 });
 
   
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
