a6f4a17579668166ff72a64ee938515f04bc4048
leah16303 <leahsarles@gmail.com>
2024-01-31 21:10:17 -0800

    implemented findUserByID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -47,21 +47,19 @@ const users = {
     ]
   };
 
-  const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-  };
   
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
-  });
+  
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
   
